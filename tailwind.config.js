f98d80d75e7d48130703b5bb3706a8196cd997a4
Thanh Pham <zlatan.pham@gmail.com>
2022-03-05 07:30:00 +0700

    chore: upgrade packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ const formPlugin = require('@tailwindcss/forms')
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
