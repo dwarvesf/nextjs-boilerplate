7898e9d72b93d1a3e88ecc4814bff8fbddfd834c
Thanh Pham <zlatan.pham@gmail.com>
2023-08-15 15:57:11 +0700

    refactor: use webpack svgr instead of own script (#35)

--- next.config.js
+++ next.config.js
@@ -12,4 +12,13 @@ module.exports = withBundleAnalyzer({
   env: {
     BASE_URL: process.env.BASE_URL,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 })
