dccf09c295f5fa9d3d9fb20b54b178b63ef62d22
An Tran <tienan92it@gmail.com>
2023-10-10 11:42:02 +0700

    chore: bypass onUnhandledRequest from nextjs server routes

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,11 +1,13 @@
 async function initMocks() {
   if (typeof window === 'undefined') {
     const { server } = await import('./server')
-    server.listen()
+    server.listen({
+      onUnhandledRequest: 'bypass', // Bypass unhandled requests on nextjs server
+    })
   } else {
     const { worker } = await import('./browser')
     worker.start({
-      onUnhandledRequest: 'bypass', // Bypass unhandled requests on nextjs routes
+      onUnhandledRequest: 'bypass', // Bypass unhandled requests on browser
     })
   }
 }
