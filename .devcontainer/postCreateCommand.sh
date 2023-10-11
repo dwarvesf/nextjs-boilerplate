110b758c527ef23632f6d5a91278037526c1fb22
Tom X Nguyen <tom81094@gmail.com>
2023-10-11 22:39:34 +0700

    perf: apply package-import-method conditionally

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# remove node_modules if it exists from the host platform
-rm -rf node_modules
+# remove node_modules and .pnpm-store if it exists from the host platform
+rm -rf node_modules .pnpm-store
 
-# remove .pnpm-store if it exists from the host platform
-rm -rf .pnpm-store
+# hard link workaround for docker containers instantiated on darwin
+# https://github.com/pnpm/pnpm/issues/7024
+if [ $(sysctl -n kern.osversion | cut -d '.' -f 1) -ge 20 ]; then
+  echo "Hard links are supported."
+else
+  echo "Hard links are not supported."
+
+  # edit a fresh .npmrc with package-import-method set to clone-or-copy and ignore git changes on it
+  git checkout master -- .npmrc
+  git update-index --assume-unchanged .npmrc
+  echo "package-import-method=clone-or-copy" >> .npmrc
+fi
 
 # install all modules with pnpm
 pnpm install
